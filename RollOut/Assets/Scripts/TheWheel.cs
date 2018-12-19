9e3178a8a389ef94eb9e9ee2615d5fffdfd6990d
3ppehr <sepehr.asghari1377@gmail.com>
2018-12-19 22:30:33 +0330

    bateSpeed

--- RollOut/Assets/Scripts/TheWheel.cs
+++ RollOut/Assets/Scripts/TheWheel.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class TheWheel : MonoBehaviour
 {
-    public static float Speed = 5f;
+    public static float Speed =5;
     public Rigidbody2D RbCharkh;
     Vector3 lastPos;
     
     void Update()
     {
+
+
+        // transform.Translate(Vector2.right * Speed * (Mathf.Sqrt(transform.position.x / 100) + 1));
+        // 
         
+       // RbCharkh.velocity = new Vector2(Speed, 0f);
 
-       // transform.Translate(Vector2.right * Speed * (Mathf.Sqrt(transform.position.x / 100) + 1));
-       // RbCharkh.velocity = (this.transform.position);
+        if (Time.deltaTime < Speed)
+        {
+            Speed += (Time.deltaTime);
+            
 
+        }
 
         RbCharkh.velocity = new Vector2(Speed, 0f);
-       // transform.Translate(Speed, 0, 0);
+        //Speed += Time.deltaTime;
+        // transform.Translate(Speed, 0, 0);
     }
 }
 
