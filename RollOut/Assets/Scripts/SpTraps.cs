e32e3c5b6fa9cf542b8f9359de6eacd9822fa9bb
3ppehr <sepehr.asghari1377@gmail.com>
2018-12-21 15:16:09 +0330

    rock fixe

--- RollOut/Assets/Scripts/SpTraps.cs
+++ RollOut/Assets/Scripts/SpTraps.cs
@@ -11,18 +11,12 @@ public class SpTraps : MonoBehaviour {
     float roundX;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Instantiate(Tr1, SpawnerPoint.transform.position, Quaternion.identity);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Time.time > nextspawn)
-        {
-
-            roundX = Random.Range(-1.66f, 2);
-            whereTospawn = new Vector2(roundX, transform.position.y);
-            Instantiate(Tr1, SpawnerPoint.transform.position, Quaternion.identity);
-        }
+        
 	}
 }
